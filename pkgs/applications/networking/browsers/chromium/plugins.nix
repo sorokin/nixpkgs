c0bbe74c0f79319f1ddf4be9f036088d44f7da24
Ivan Sorokin <vanyacpp@gmail.com>
2017-09-19 02:54:28 +0300

    flash: 25.0.0.127 -> 27.0.0.130

--- pkgs/applications/networking/browsers/chromium/plugins.nix
+++ pkgs/applications/networking/browsers/chromium/plugins.nix
@@ -94,12 +94,12 @@ let
 
   flash = stdenv.mkDerivation rec {
     name = "flashplayer-ppapi-${version}";
-    version = "25.0.0.127";
+    version = "27.0.0.130";
 
     src = fetchzip {
       url = "https://fpdownload.adobe.com/pub/flashplayer/pdc/"
           + "${version}/flash_player_ppapi_linux.x86_64.tar.gz";
-      sha256 = "1gf0ncclkk3h4vj9kkhbqj1nnnm54gwm5mdcs4p4pl8i339scs14";
+      sha256 = "005sxx3ll18c6idy1db2gb47chd9c5mf83qac0vyvljsrlc7430c";
       stripRoot = false;
     };
 
